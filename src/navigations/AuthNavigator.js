27c37885eeb65da71c7591fc057967c4cef61d71
HoangQuocHung0209 <92779061+HQHung0209@users.noreply.github.com>
2023-03-29 15:54:34 +0700

    import library

--- src/navigations/AuthNavigator.js
+++ src/navigations/AuthNavigator.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {Login, Register, Loading} from '../screens';
-import {ROUTES} from '../constants';
-import {Colors} from '../constants/colors';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Login, Register, Loading } from '../screens';
+import { ROUTES } from '../constants';
+import { Colors } from '../constants/colors';
 import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createStackNavigator();
@@ -12,7 +12,7 @@ function AuthNavigator() {
   console.log('AuthNavigator', Stack);
   return (
     <Stack.Navigator
-      screenOptions={({route}) => ({
+      screenOptions={({ route }) => ({
         headerTintColor: Colors.WHITE,
         headerBackTitleVisible: false,
         headerStyle: {
@@ -20,10 +20,11 @@ function AuthNavigator() {
         },
       })}
       initialRouteName={ROUTES.LOGIN}>
+      <Stack.Screen name={ROUTES.LOADING} component={Loading} />
       <Stack.Screen name={ROUTES.REGISTER} component={Register} />
       <Stack.Screen name={ROUTES.LOGIN} component={Login} />
       <Stack.Screen
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
         name={ROUTES.HOME}
         component={BottomTabNavigator}
       />
