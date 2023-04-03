975ae0f5229df0706b7d282ff12c2b274a461623
poul2712 <82167965+huyduc2712@users.noreply.github.com>
2023-04-03 11:02:28 +0700

    HomeShop

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,13 @@
 const icons = {
   icon_LOGO: require('../assets/icons/Logo.png'),
+  icon_Crown: require('../assets/icons/Crown.png'),
+  icon_TagSale: require('../assets/icons/Tag.png'),
 };
 
 const images = {
   img_Background: require('../assets/images/Backgound.png'),
+  img_User: require('../assets/images/User.png'),
+  img_Item: require('../assets/images/Item.png'),
 };
 
 export {icons, images};
